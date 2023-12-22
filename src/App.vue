5c0a73eff53697835d5b72b637962b3cf2d517f9
Shamble94 <fratus.nicholas@gmail.com>
2023-12-22 09:50:58 +0100

    Correzione esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script setup>
+<script>
 import AppTitle from './components/AppTitle.vue'
 import AppHero from  './components/AppHero.vue'
+
+export default {
+  components: {
+    AppTitle,
+    AppHero
+  }
+    
+}
 </script>
-
-
 <template>
   <div>
     <AppTitle>
